da27d5490395bc346be2324a3dde05fc8e21d7cb
Matte <folkol@mailbox.org>
2025-01-02 15:05:20 +0100

    reset the example to the normal way

--- examples/example-benchmark.js
+++ examples/example-benchmark.js
@@ -1,19 +1,17 @@
-import {bench, Criterion, group} from '../index.js';
+import {bench, Criterion} from '../index.js';
 import {fibonacciAsync, fibonacciIterative, fibonacciRecursive} from "./funcs.js";
 
 let criterion = new Criterion({
-    measurementTime: 0.1,
-    nResamples: 10,
-    warmUpTime: 0.2,
+    //measurementTime: 0.1,
+    //nResamples: 10,
+    //warmUpTime: 0.2,
 });
 
-bench('Hello', () => {})
+let group = criterion.group('Defualt');
+group.bench('Hello', () => {})
 
-group('Fibonacci', () => {
-    bench('Iterative', fibonacciIterative, 15)
-    bench('Recursive', fibonacciRecursive, 15)
+let fibonacci = criterion.group('Ficonacci');
 
-    group('Nested', () => {
-        bench('Test', async () => {}, 15)
-    })
-})
+fibonacci.bench('Iterative', fibonacciIterative, 15);
+fibonacci.bench('Iterative (async)', fibonacciAsync, 15);
+fibonacci.bench('Recursive', fibonacciRecursive, 15);
